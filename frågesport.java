e71cf2c657c4eafb177656a4998e0cf0022c4679
AleksandraTE22C <aleksandra.kajak-tonkiel@elev.ga.ntig.se>
2023-10-16 09:34:39 +0200

    Klar med frågesport (typ)

---
+++ frågesport.java
@@ -0,0 +1,168 @@
+import java.util.Scanner;
+public class frågesport {
+    public static void main(String args[]){
+        Scanner tb = new Scanner (System.in);
+
+        int poäng = 0;
+        System.out.println("Fråga 1");
+        System.out.println("alt1, alt2, alt3, alt4");
+        System.out.println("Vilket är rätt? Skriv numret:");
+        int val1 = tb.nextInt();
+
+        switch(val1){
+            case 1: 
+            System.out.println("Rätt");
+            poäng+=1;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+
+            case 2:
+            System.out.println("Fel");
+            poäng+=0;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+            
+            case 3:
+            System.out.println("Fel");
+            poäng+=0;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+            
+            case 4:
+            System.out.println("Fel");
+            poäng+=0;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+
+        }
+        System.out.println("");
+        System.out.println("Fråga 2");
+        System.out.println("alt1, alt2, alt3, alt4");
+        System.out.println("Vilket är rätt? Skriv numret:");
+        int val2 = tb.nextInt();
+
+        switch(val2){
+            case 1: 
+            System.out.println("Fel");
+            poäng+=0;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+
+            case 2:
+            System.out.println("Fel");
+            poäng+=0;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+            
+            case 3:
+            System.out.println("Rätt");
+            poäng+=1;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+            
+            case 4:
+            System.out.println("Fel");
+            poäng+=0;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+
+        }
+        System.out.println("");
+        System.out.println("Fråga 3");
+        System.out.println("alt1, alt2, alt3, alt4");
+        System.out.println("Vilket är rätt? Skriv numret:");
+        int val3 = tb.nextInt();
+
+        switch(val3){
+            case 1: 
+            System.out.println("Fel");
+            poäng+=0;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+
+            case 2:
+            System.out.println("Rätt");
+            poäng+=1;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+            
+            case 3:
+            System.out.println("Fel");
+            poäng+=0;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+            
+            case 4:
+            System.out.println("Fel");
+            poäng+=0;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+
+        }
+        System.out.println("");
+        System.out.println("Fråga 4");
+        System.out.println("alt1, alt2, alt3, alt4");
+        System.out.println("Vilket är rätt? Skriv numret:");
+        int val4 = tb.nextInt();
+
+        switch(val4){
+            case 1: 
+            System.out.println("Fel");
+            poäng+=0;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+
+            case 2:
+            System.out.println("Fel");
+            poäng+=0;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+            
+            case 3:
+            System.out.println("Rätt");
+            poäng+=1;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+            
+            case 4:
+            System.out.println("Fel");
+            poäng+=0;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+
+        }
+        System.out.println("");
+        System.out.println("Fråga 5");
+        System.out.println("alt1, alt2, alt3, alt4");
+        System.out.println("Vilket är rätt? Skriv numret:");
+        int val5 = tb.nextInt();
+
+        switch(val5){
+            case 1: 
+            System.out.println("Fel");
+            poäng+=0;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+
+            case 2:
+            System.out.println("Fel");
+            poäng+=0;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+            
+            case 3:
+            System.out.println("Fel");
+            poäng+=0;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+            
+            case 4:
+            System.out.println("Rätt");
+            poäng+=1;
+            System.out.println("Du har "+poäng+" poäng");
+            break;
+
+        }
+        System.out.println("Du fick "+poäng+"/5 poäng");
+    }
+}
